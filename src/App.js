0517a6b8c3bdbf98315b4ce5b98715497e5c785b
Yener Aydemir <yenerlie@gmail.com>
2024-01-08 18:12:12 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Header from './components/header';
+import Sectwo from './components/sectwo';
+import Secone from './components/secone';
+import Footer from './components/footer';
+import Sectre from './components/sectre';
+import Secfour from './components/secfour';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Sectwo />
+      <Secfour />
+      <Secone />
+      <Sectre />
+      <Footer />
+
     </div>
   );
-}
+};
 
 export default App;
